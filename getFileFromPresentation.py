6c54b4c29d23cffdcf261557f5bddbb21909a50a
Zimogor70 <43498740+Zimogor70@users.noreply.github.com>
2024-11-04 10:45:35 +0300

    Задача 2024.10.25.02 доб. комменты

--- getFileFromPresentation.py
+++ getFileFromPresentation.py
@@ -2,11 +2,16 @@
 Откройте файл презентации в программе с помощью объекта Presentation библиотеки pptx
 '''
 import zipfile
-from pptx import Presentation
+from pptx import Presentation # подключаем класс Перс... из пптикс библиотеки
 
+# получаем ссылку на файл с презентацией
 prs = Presentation('src\Zimnyaya_igra_1.pptx')
+# бежим по слайдам
 for slide in prs.slides:
+    # бежим по шейпам на каждом слайде
     for shape in slide.shapes:
+        # если шейп на слайде пустой, то идем к следующему шейпу
         if not shape.has_text_frame:
             continue
-        print(shape.text)
+        # выводим в консоль содержимое фрейма
+        print(shape.text_frame)
