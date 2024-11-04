c93bbb6d4d6aab23e7b1381f5874a9ff5fecc82f
Zimogor70 <43498740+Zimogor70@users.noreply.github.com>
2024-11-04 11:07:50 +0300

    Задача 2024.10.25.04

--- getFileFromPresentation.py
+++ getFileFromPresentation.py
@@ -1,15 +1,15 @@
-'''Задача 2024.10.25.03
-Поменяйте код так, чтобы он выводил текст каждого шэйпа на экран.'''
-from pptx import Presentation # подключаем класс Перс... из пптикс библиотеки
+'''Задача 2024.10.23.04
+Сохраните из этой презентации только слова,
+ всякие "Правила", "Тур 1" и другие объяснение не нужны.
+Сохраните все слова в файл words.txt'''
+from pptx import Presentation
 
-# получаем ссылку на файл с презентацией
-prs = Presentation('src\Zimnyaya_igra_1.pptx')
-# бежим по слайдам
-for slide in prs.slides:
-    # бежим по шейпам на каждом слайде
-    for shape in slide.shapes:
-        # если шейп на слайде пустой, то идем к следующему шейпу
-        if not shape.has_text_frame:
-            continue
-        # выводим в консоль содержимое фрейма
-        print(shape.text_frame.text)
+with open('words.txt','w',encoding="utf8") as file:
+    file.write('')
+    prs = Presentation('src\Zimnyaya_igra_1.pptx')
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if not shape.has_text_frame:
+                continue
+            file.write(shape.text_frame.text)
+
