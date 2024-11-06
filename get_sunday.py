06fef4d91bf0d7c33bc536902c69e00303ed40fb
Zimogor70 <43498740+Zimogor70@users.noreply.github.com>
2024-11-06 19:53:13 +0300

    Задача 2024.11.06.01 Попробуйте написать даты, когда проходила каждая игра, учитывая что игры проходили по воскресеньям.

--- get_sunday.py
+++ get_sunday.py
@@ -1,17 +1,22 @@
 '''Задача 2024.11.06.01
 Попробуйте написать даты, когда проходила каждая игра,
 учитывая что игры проходили по воскресеньям.'''
+import datetime
+import os
 
-from datetime import date, timedelta
+path = "src\Zimnyaya_igra_1.pptx"
 
+# file modification
+timestamp = os.path.getmtime(path)
 
-def allsundays(year):
-    d = date(year, 1, 1)
-    d += timedelta(days=6 - d.weekday())
-    while d.year == year:
-        yield d
-        d += timedelta(days=7)
+# convert timestamp into DateTime object
+datestamp = datetime.datetime.fromtimestamp(timestamp)
+print('Modified Date/Time:', datestamp)
 
+# file creation
+c_timestamp = os.path.getctime(path)
 
-for d in allsundays(2024):
-    print(d)
+# convert creation timestamp into DateTime object
+c_datestamp = datetime.datetime.fromtimestamp(c_timestamp)
+
+print('Created Date/Time on:', c_datestamp)
