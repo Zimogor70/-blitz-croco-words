eb275bbcbf1f181c8e95844858d1892398edb6c0
Zimogor70 <43498740+Zimogor70@users.noreply.github.com>
2024-11-15 18:58:42 +0300

    add crate table movie

--- db.py
+++ db.py
@@ -4,7 +4,8 @@ import sqlite3
 def main():
     con = sqlite3.connect('words.db')
     cur = con.cursor()
+    cur.execute('CREATE TABLE movie(title, year, score)')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
