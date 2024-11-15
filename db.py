f57420b7d77631df3f596f74125a1a5963735414
Zimogor70 <43498740+Zimogor70@users.noreply.github.com>
2024-11-15 20:43:32 +0300

    add comments

--- db.py
+++ db.py
@@ -2,9 +2,22 @@ import sqlite3
 
 
 def main():
-    con = sqlite3.connect('words.db')
-    cur = con.cursor()
-    cur.execute('CREATE TABLE movie(title, year, score)')
+    con = sqlite3.connect('words.db')  # соединение с базой или создание, если таковой нет
+    cur = con.cursor()  # получаем курсор базы
+    # cur.execute('CREATE TABLE IF NOT EXISTS movie(title, year, score)')
+    # res = cur.execute('SELECT name FROM sqlite_master') # get link on object classes of cursor
+    # print(res.fetchone()) # получаем кортеж данных отработавшего запроса
+    # исполяем запрос на создание таблицы, если нет таковой
+    cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
+    # в списке храним записи для последующего добавления в таблицу
+    data = [
+        ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+        ("Monty Python's The Meaning of Life", 1983, 7.5),
+        ("Monty Python's Life of Brian", 1979, 8.0),
+    ]
+    # исполняем запрос на добавление записей из data
+    cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+    con.commit()  # зафиксировать результаты транзакции в БД
 
 
 if __name__ == '__main__':
